e805ad0ca9d2ffc61b3239c759f66c16236a6c69
Malte Schröder <malte.schroeder@tnxip.de>
2020-12-28 14:53:41 +0100

    OBD: convert to async. Still missing timeout handling

--- code/src/obd.cpp
+++ code/src/obd.cpp
@@ -17,6 +17,20 @@ struct can_frame {
 	uint8_t data[CAN_MAX_FRAME_LEN];
 };
 
+union bit_field {
+	uint8_t value;
+	uint8_t bit1 : 1;
+	uint8_t bit2 : 1;
+	uint8_t bit3 : 1;
+	uint8_t bit4 : 1;
+	uint8_t bit5 : 1;
+	uint8_t bit6 : 1;
+	uint8_t bit7 : 1;
+	uint8_t bit8 : 1;
+};
+
+bool IsoTpInitialized = false;
+
 int read_frame(uint32_t can_rx, uint16_t timeout, struct can_frame* buffer) {
 	// Busy wait for response
 
@@ -38,8 +52,98 @@ int read_frame(uint32_t can_rx, struct can_frame* buffer) {
 	return read_frame(can_rx, CAN_DEFAULT_READ_TIMEOUT, buffer);
 }
 
+struct {
+	size_t data_buf_len;
+	size_t msg_len;
+	uint32_t can_tx;
+	uint16_t data_offset;
+	uint8_t last_frame_idx;
+	uint8_t data_buf[ISOTP_MAX_MSG_LEN];
+	void (*isotp_callback)(uint8_t *, size_t);
+	void (*frame_callback)(struct can_frame *);
+	unsigned int can_receive_registered : 1;
+	unsigned int transaction_running : 1;
+} IsoTpState;
+
+void isotp_receive(int packet_size) {
+	struct can_frame frame;
+
+	assert(packet_size <= CAN_MAX_FRAME_LEN);
+
+	frame.can_id = CAN.packetId();
+	frame.length = packet_size;
+	size_t bytes_read = CAN.readBytes((char*)&frame.data, frame.length);
+	assert(bytes_read == frame.length);
+#if DEBUG
+	Serial.print("Received frame: ");
+	hexdump(frame.data, frame.length, 8);
+#endif
+
+	// XXX: how to handle timouts / missed frames, leading to transaction not ending ??
+
+	if (IsoTpState.transaction_running) {
+		switch (frame.data[0] & 0xf0) {  // frame_type
+			case 0x00:                    // single frame
+				IsoTpState.msg_len = frame.data[0] & 0x0f;
+				assert(IsoTpState.msg_len <= ISOTP_MAX_MSG_LEN);
+				memcpy(IsoTpState.data_buf, frame.data + 1,
+						 min(IsoTpState.msg_len, ISOTP_MAX_MSG_LEN));
+				IsoTpState.data_offset = IsoTpState.msg_len;
+				break;
+			case 0x10:                    // first frame
+				assert(IsoTpState.data_offset == 0);  // if data_offset != 0 we have already seen the first frame
+				IsoTpState.msg_len = (frame.data[0] & 0x0f) + frame.data[1];
+				assert(IsoTpState.msg_len <= ISOTP_MAX_MSG_LEN);
+				memcpy(IsoTpState.data_buf, frame.data + 2,
+						 min(frame.length - 2, ISOTP_MAX_MSG_LEN));
+				IsoTpState.data_offset = frame.length - 2;
+				IsoTpState.last_frame_idx = 0;
+#if DEBUG
+				Serial.print("Send frame:     ");
+				hexdump((uint8_t*)&CanFlowMsg, sizeof(CanFlowMsg), 8);
+#endif
+				CAN.beginPacket(IsoTpState.can_tx);
+				CAN.write(CanFlowMsg, sizeof(CanFlowMsg));
+				CAN.endPacket();
+				break;
+			case 0x20:                    // continuation frame
+				uint8_t frame_idx = frame.data[0] & 0x0f;
+				assert((IsoTpState.last_frame_idx + 1) % 0x10 == frame_idx); // misordered frame received ?!?
+				size_t payload_len = min(7, (uint16_t)IsoTpState.msg_len - IsoTpState.data_offset);
+				if (IsoTpState.data_offset < ISOTP_MAX_MSG_LEN)	// Avoid copying past the target buffer
+					memcpy(IsoTpState.data_buf + IsoTpState.data_offset, frame.data + 1,
+						 	min(payload_len, ISOTP_MAX_MSG_LEN - IsoTpState.data_offset));
+				IsoTpState.data_offset += payload_len;
+				IsoTpState.last_frame_idx = frame_idx;
+				break;
+			case 0x30:                    // flow control frame
+				// Flow control frame, cannot handle, since we are asking we should never see this
+				assert(false);
+				break;
+		}
+
+		if (IsoTpState.data_offset == IsoTpState.msg_len) {
+			if (IsoTpState.isotp_callback) {	// Thou shalt not follow the NULL pointer!
+				IsoTpState.isotp_callback(IsoTpState.data_buf, ISOTP_MAX_MSG_LEN);
+				IsoTpState.transaction_running = false;
+				CAN.filter(0, 0);	// open receiver for broadcast frames like on Zoe
+			}
+		}
+	}
+	else {	// received "free" can frame
+		if (IsoTpState.frame_callback)	// Thou shalt not follow the NULL pointer!
+			IsoTpState.frame_callback(&frame);
+	}
+}
+
 int isotp_cmd(uint32_t can_rx, uint32_t can_tx, uint8_t* cmd, size_t cmd_len,
-              uint8_t* data_buf, size_t data_buf_len) {
+              void (*callback)(uint8_t *, size_t)) {
+
+	if (!IsoTpInitialized) {
+		memset(&IsoTpState, 0, sizeof(IsoTpState));
+		CAN.onReceive(isotp_receive);
+		IsoTpInitialized = true;
+	}
 
 	assert(cmd_len < CAN_MAX_FRAME_LEN);
 
@@ -55,60 +159,29 @@ int isotp_cmd(uint32_t can_rx, uint32_t can_tx, uint8_t* cmd, size_t cmd_len,
 	hexdump(frame.data, frame.length, 8);
 #endif
 
+	while(IsoTpState.transaction_running);	// block if another transaction is already running
+
+	CAN.filter(can_rx, 0x7ff);
+
+	IsoTpState.can_tx = can_tx;
+	IsoTpState.isotp_callback = callback;
+	IsoTpState.transaction_running = true;
+
 	CAN.beginPacket(can_tx);
 	CAN.write(frame.data, frame.length);
 	CAN.endPacket();
+}
 
-	size_t msg_len = 0;         // length of all frames combined
-	size_t payload_len = 0;     // length of iso-tp payload of current frame
-	uint8_t last_frame_idx = 0;
-	uint8_t frame_idx = 0;
-	uint16_t data_offset = 0;
-	while(data_offset < msg_len || msg_len == 0) {
-		// We need to read all frames from buffer. Too small target buffers are handled
-		read_frame(can_rx, &frame);
-#if DEBUG
-		Serial.print("Received frame: ");
-		hexdump(frame.data, frame.length, 8);
-#endif
+int isotp_cmd(uint32_t can_rx, uint32_t can_tx, uint8_t* cmd, size_t cmd_len,
+              uint8_t* data_buf, size_t data_buf_len) {
+   isotp_cmd(can_rx, can_tx, cmd, cmd_len, NULL);
 
-		switch (frame.data[0] & 0xf0) {  // frame_type
-			case 0x00:                    // single frame
-				msg_len = frame.data[0] & 0x0f;
-				memcpy(data_buf, frame.data + 1, min(msg_len, data_buf_len));
-				data_offset = msg_len;
-				break;
-			case 0x10:                    // first frame
-				assert(data_offset == 0);  // if data_offset != 0 we have already seen the first frame
-				msg_len = (frame.data[0] & 0x0f) + frame.data[1];
-				memcpy(data_buf, frame.data + 2, min(frame.length - 2, data_buf_len));
-				data_offset = frame.length - 2;
-				last_frame_idx = 0;
-#if DEBUG
-				Serial.print("Send frame:     ");
-				hexdump((uint8_t*)&CanFlowMsg, sizeof(CanFlowMsg), 8);
-#endif
-				CAN.beginPacket(can_tx);
-				CAN.write(CanFlowMsg, sizeof(CanFlowMsg));
-				CAN.endPacket();
-				break;
-			case 0x20:                    // continuation frame
-				frame_idx = frame.data[0] & 0x0f;
-				assert((last_frame_idx + 1) % 0x10 == frame_idx); // misordered frame received ?!?
-				payload_len = min(7, (uint16_t)msg_len - data_offset);
-				if (data_offset < data_buf_len)	// Avoid copying past the target buffer
-					memcpy(data_buf + data_offset, frame.data + 1, min(payload_len, data_buf_len - data_offset));
-				data_offset += payload_len;
-				last_frame_idx = frame_idx;
-				break;
-			case 0x30:                    // flow control frame
-				// Flow control frame, cannot handle, since we are asking we should never see this
-				assert(false);
-				break;
-		}
-	}
+	while(IsoTpState.transaction_running);	// wait for end of transaction
 
-	assert(data_offset == msg_len);	// Check if we got the amount of data we expected from the first frame
+	memcpy(data_buf, IsoTpState.data_buf, min(data_buf_len, IsoTpState.msg_len));
+
+   IsoTpState.transaction_running = false;
+	CAN.filter(0, 0);	// open receiver for broadcast frames like on Zoe
 }
 
 void car_decoder(float *evn, struct vehicle_d *vehicle) {
--- code/src/obd.h
+++ code/src/obd.h
@@ -2,12 +2,14 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-#define CAN_PIN_RX 4
-#define CAN_PIN_TX 5
-#define CAN_BUS_SPEED 500E3
+const unsigned short CAN_PIN_RX = 4;
+const unsigned short CAN_PIN_TX = 5;
+const unsigned long CAN_BUS_SPEED = 500E3;
 
-#define CAN_MAX_FRAME_LEN 8
-#define CAN_DEFAULT_READ_TIMEOUT 500
+const size_t CAN_MAX_FRAME_LEN = 8;
+const int CAN_DEFAULT_READ_TIMEOUT = 500;
+
+const size_t ISOTP_MAX_MSG_LEN = 128;	// twice as much as the biggest structure currently known
 
 int isotp_cmd(uint32_t can_rx, uint32_t can_tx, uint8_t* cmd, size_t cmd_len,
               uint8_t* data_buf, size_t data_len);
